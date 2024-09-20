45b503c6acfe312b1427cd1c044e3c88347b22b3
Sumangal44 <123644743+Sumangal44@users.noreply.github.com>
2024-09-21 01:01:29 +0530

    add ansi color codes🚀

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <conio.h>
+// ANSI color codes
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define RESET "\033[0m" // Reset color
+
 
 void displayBanner();
 void sum(int a, int b);
