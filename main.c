8ab3da510ed6d8a9eebf285d4edd06d0d5c74e7f
Sumangal44 <123644743+Sumangal44@users.noreply.github.com>
2024-09-21 00:35:14 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+void displayBanner();
+void sum(int a, int b);
+void sub(int a, int b);
+void mul(int a, int b);
+void div(int a, int b);
+void rem(int a, int b);
+
+int main()
+{
+    int a, b, choice;
+    return 0;
+}
