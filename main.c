e911c86145891d126fb2729fee4f8950d435607e
Sumangal44 <123644743+Sumangal44@users.noreply.github.com>
2024-09-21 00:49:47 +0530

    add sub funtionand mul funtion🚀

--- main.c
+++ main.c
@@ -70,3 +70,13 @@ void sum(int a, int b)
 {
     printf("%d + %d = %d\n", a, b, a + b);
 }
+void sub(int a, int b)
+{
+    printf("%d - %d = %d\n", a, b, a - b);
+}
+
+void mul(int a, int b)
+{
+    printf("%d * %d = %d\n", a, b, a * b);
+}
+
