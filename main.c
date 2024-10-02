e17453526070542ab8cc3bc31ee0f6321599318e
Sumangal44 <123644743+Sumangal44@users.noreply.github.com>
2024-10-02 15:59:06 +0530

    added new update 🚀🚀🚀

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
+#include <math.h> // For advanced math functions
+#define M_PI 3.14159265358979323846
+
 // ANSI color codes
 #define RED "\033[31m"
 #define GREEN "\033[32m"
@@ -11,15 +14,23 @@
 #define RESET "\033[0m" // Reset color
 
 void displayBanner();
-void sum(int a, int b);
-void sub(int a, int b);
-void mul(int a, int b);
-void divi(int a, int b);
+void sum(double a, double b);
+void sub(double a, double b);
+void mul(double a, double b);
+void divi(double a, double b);
 void rem(int a, int b);
+void power(double base, double exp);
+void sqroot(double a);
+void sin_func(double angle);
+void cos_func(double angle);
+void tan_func(double angle);
+void log_func(double a);
+
 
 int main()
 {
-    int a, b, choice;
+    int choice;
+    double a, b;
     system("cls");
 
     while (1) // Add loop to keep asking for inputs until exit is chosen
@@ -28,23 +39,54 @@ int main()
         displayBanner();
 
         printf(YELLOW "Enter your choice\n" RESET);
-        printf(GREEN "1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Remainder\n6.Exit\n" RESET);
+        printf(GREEN "1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Remainder\n");
+        printf("6. Power\n7. Square Root\n8. Sine\n9. Cosine\n10. Tangent\n11. Logarithm\n12. Exit\n" RESET);
         scanf("%d", &choice);
 
-        if (choice == 6) // Exit condition
+        if (choice == 12) // Exit condition
         {
             printf(CYAN "Exiting the program.....\n" RESET);
             break;
         }
 
-        if (choice < 1 || choice > 6)
+        if (choice < 1 || choice > 12)
         {
-            printf(RED "Select within the range!\n" RESET);
+            printf(RED "Invalid choice! Please select within the range\n" RESET);
             continue; // Go back to the menu if choice is invalid
         }
 
-        printf(YELLOW "Enter two integer numbers\n" RESET);
-        scanf("%d%d", &a, &b);
+switch (choice)
+        {
+        case 1: case 2: case 3: case 4:
+            printf(YELLOW "Enter two numbers:\n" RESET);
+            scanf("%lf %lf", &a, &b);
+            break;
+        case 5:
+            printf(YELLOW "Enter two integers for remainder:\n" RESET);
+            int x, y;
+            scanf("%d %d", &x, &y);
+            rem(x, y);
+            continue;
+        case 6:
+            printf(YELLOW "Enter the base and exponent:\n" RESET);
+            scanf("%lf %lf", &a, &b);
+            power(a, b);
+            continue;
+        case 7:
+            printf(YELLOW "Enter a number for square root:\n" RESET);
+            scanf("%lf", &a);
+            sqroot(a);
+            continue;
+        case 8: case 9: case 10:
+            printf(YELLOW "Enter the angle in degrees:\n" RESET);
+            scanf("%lf", &a);
+            break;
+        case 11:
+            printf(YELLOW "Enter a number for logarithm:\n" RESET);
+            scanf("%lf", &a);
+            log_func(a);
+            continue;
+        }
 
         switch (choice)
         {
@@ -60,9 +102,16 @@ int main()
         case 4:
             divi(a, b);
             break;
-        case 5:
-            rem(a, b);
+        case 8:
+            sin_func(a);
             break;
+        case 9:
+            cos_func(a);
+            break;
+        case 10:
+            tan_func(a);
+            break;
+        
         default:
             printf(RED "You entered a wrong choice\n" RESET);
             break;
@@ -74,29 +123,29 @@ int main()
 void displayBanner()
 {
     printf(BLUE "***************************************\n" RESET);
-    printf(MAGENTA "*          BASIC CALCULATOR           *\n" RESET);
+    printf(MAGENTA "*      ADVANCED SCIENTIFIC CALCULATOR  *\n" RESET);
     printf(MAGENTA "*         Made in C Language          *\n" RESET);
     printf(BLUE "***************************************\n" RESET);
 }
-void sum(int a, int b)
+void sum(double a, double b)
 {
-    printf(GREEN "%d + %d = %d\n" RESET, a, b, a + b);
+    printf(GREEN "%.2lf + %.2lf = %.2lf\n" RESET, a, b, a + b);
 }
-void sub(int a, int b)
+void sub(double a, double b)
 {
-    printf(GREEN "%d - %d = %d\n" RESET, a, b, a - b);
-}
-
-void mul(int a, int b)
-{
-    printf(GREEN "%d * %d = %d\n" RESET, a, b, a * b);
+    printf(GREEN "%.2lf - %.2lf = %.2lf\n" RESET, a, b, a - b);
 }
 
-void divi(int a, int b)
+void mul(double a, double b)
+{
+    printf(GREEN "%.2lf * %.2lf = %.2lf\n" RESET, a, b, a * b);
+}
+
+void divi(double a, double b)
 {
     if (b != 0)
     {
-        printf(GREEN "%d / %d = %d\n" RESET, a, b, a / b);
+        printf(GREEN "%.2lf / %.2lf = %.2lf\n" RESET, a, b, a / b);
     }
     else
     {
@@ -112,5 +161,48 @@ void rem(int a, int b)
     else
     {
         printf(RED "Modulus by zero is not allowed!\n" RESET);
+    }
+}
+void power(double base, double exp)
+{
+    printf(GREEN "%.2lf ^ %.2lf = %.2lf\n" RESET, base, exp, pow(base, exp));
+}
+
+void sqroot(double a)
+{
+    if (a >= 0)
+    {
+        printf(GREEN "%.2lf = %.2lf\n" RESET, a, sqrt(a));
+    }
+    else
+    {
+        printf(RED "Square root of a negative number is not possible!\n" RESET);
+    }
+}
+
+void sin_func(double angle)
+{
+    printf(GREEN "sin(%.2lf degrees) = %.2lf\n" RESET, angle, sin(angle * M_PI / 180));
+}
+
+void cos_func(double angle)
+{
+    printf(GREEN "cos(%.2lf degrees) = %.2lf\n" RESET, angle, cos(angle * M_PI / 180));
+}
+
+void tan_func(double angle)
+{
+    printf(GREEN "tan(%.2lf degrees) = %.2lf\n" RESET, angle, tan(angle * M_PI / 180));
+}
+
+void log_func(double a)
+{
+    if (a > 0)
+    {
+        printf(GREEN "log(%.2lf) = %.2lf\n" RESET, a, log(a));
+    }
+    else
+    {
+        printf(RED "Logarithm of non-positive numbers is not defined!\n" RESET);
     }
 }
